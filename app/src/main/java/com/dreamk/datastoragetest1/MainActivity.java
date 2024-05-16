706646de6a1e9b063e15dbcc602b00c50dadaea5
DreamK2826 <jurui2826@qq.com>
2024-05-16 22:18:34 +0800

    保存数据到sp测试1

--- app/src/main/java/com/dreamk/datastoragetest1/MainActivity.java
+++ app/src/main/java/com/dreamk/datastoragetest1/MainActivity.java
@@ -1,6 +1,10 @@
 package com.dreamk.datastoragetest1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sp;
+    EditText et1;
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        sp = getSharedPreferences("config1", Context.MODE_PRIVATE);
+
+        et1 = findViewById(R.id.et1);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        //判断是否保存过
+        boolean flag1 = sp.getBoolean("isSaved",false);
+        if(flag1){
+            String str = sp.getString("value1","");
+            et1.setText(str);
+        }
+
+
+        btn1.setOnClickListener(v -> {
+            String string = et1.getText().toString();
+
+            if(!string.isEmpty()){
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("value1", string);
+                editor.putBoolean("isSaved", true);
+                editor.apply();
+            }
+
+        });
+
+        btn2.setOnClickListener(v -> {
+
         });
     }
 }
