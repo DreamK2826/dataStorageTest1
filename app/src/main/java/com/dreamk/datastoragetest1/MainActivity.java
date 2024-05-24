911c631c3017e03f83c576ec67178e047bf9b189
DreamK2826 <jurui2826@qq.com>
2024-05-24 16:40:44 +0800

    保存数据到SQLite测试1.00

--- app/src/main/java/com/dreamk/datastoragetest1/MainActivity.java
+++ app/src/main/java/com/dreamk/datastoragetest1/MainActivity.java
@@ -2,6 +2,9 @@ package com.dreamk.datastoragetest1;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +18,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sp;
-    EditText et1;
-    Button btn1,btn2;
+    EditText et1,et_id,et_username,et_uNumber,et_uColor,et_uMessage,et_permitted,et_value1;
+    Button btn1,btn2,btn3,btn5,btn4,clear_all_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,18 @@ public class MainActivity extends AppCompatActivity {
         et1 = findViewById(R.id.et1);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        clear_all_et = findViewById(R.id.clear_all_et);
+
+        et_value1 = findViewById(R.id.et_value1);
+        et_permitted = findViewById(R.id.et_permitted);
+        et_uMessage = findViewById(R.id.et_uMessage);
+        et_uColor = findViewById(R.id.et_uColor);
+        et_uNumber = findViewById(R.id.et_uNumber);
+        et_username = findViewById(R.id.et_username);
+        et_id = findViewById(R.id.et_id);
 
         //判断是否保存过
         boolean flag1 = sp.getBoolean("isSaved",false);
@@ -41,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             String str = sp.getString("value1","");
             et1.setText(str);
         }
+
+        clear_all_et.setOnClickListener(v -> {
+            et_value1.setText("");
+            et_permitted.setText("");
+            et_uMessage.setText("");
+            et_uColor.setText("");
+            et_uNumber.setText("");
+            et_username.setText("");
+            et_id.setText("");
+        });
 
 
         btn1.setOnClickListener(v -> {
@@ -58,5 +83,100 @@ public class MainActivity extends AppCompatActivity {
         btn2.setOnClickListener(v -> {
 
         });
+        //创建数据库按键
+        btn3.setOnClickListener(v -> {
+
+            SQLiteOpenHelper helper = MySqliteOpenHelper.getmInstance(this);
+            SQLiteDatabase writableDatabase = helper.getWritableDatabase();
+
+            writableDatabase.close();
+            helper.close();
+
+        });
+
+        btn4.setOnClickListener(v -> {
+
+            //插入数据库按键
+            SQLiteOpenHelper helper = MySqliteOpenHelper.getmInstance(this);
+            SQLiteDatabase writableDatabase = helper.getWritableDatabase();
+
+            if(writableDatabase.isOpen()){
+//                String str_id = et_id.getText().toString();
+                String str_username = et_username.getText().toString();
+                String str_uNumber = et_uNumber.getText().toString();
+//                String str_ = et_id.getText().toString();
+//                String str_id = et_id.getText().toString();
+//                String str_id = et_id.getText().toString();
+//                String str_id = et_id.getText().toString();
+                int index0;
+
+//                if(!str_id.isEmpty()){
+//                    index0 = Integer.getInteger(str_id);
+//                }
+
+                String sql1 = "insert into myTable1(username,uNumber) values('" + str_username + "','" + str_uNumber + "')";
+
+                writableDatabase.execSQL(sql1);
+
+            }
+
+            writableDatabase.close();
+            helper.close();
+        });
+
+        btn5.setOnClickListener(v -> {
+            //读取数据库按键
+            SQLiteOpenHelper helper = MySqliteOpenHelper.getmInstance(this);
+            SQLiteDatabase db = helper.getReadableDatabase();
+
+
+            if(db.isOpen()){
+                String str_id = et_id.getText().toString();
+//                int _idEt;
+                int username_index;
+                int uNumber_index;
+                String username;
+                String uNumber;
+
+                try {
+                    Cursor cursor = db.rawQuery("select * from myTable1 where _id = " + str_id,null);
+                    while (cursor.moveToNext()){
+
+                        username_index = cursor.getColumnIndex("username");
+                        uNumber_index = cursor.getColumnIndex("uNumber");
+
+
+
+                        if(username_index >= 0){
+                            username = cursor.getString(username_index);
+                        } else {
+                            username = null;
+                        }
+
+                        if(uNumber_index >= 0){
+                            uNumber = cursor.getString(uNumber_index);
+                        } else {
+                            uNumber = null;
+                        }
+
+                        et_username.setText(username);
+                        et_uNumber.setText(uNumber);
+
+                    }
+                    cursor.close();
+                } catch (Exception ignored){
+
+                    ToastUtil.show(this,"Error!");
+                }
+
+                db.close();
+            }
+
+
+
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/dreamk/datastoragetest1/MySqliteOpenHelper.java
@@ -0,0 +1,35 @@
+package com.dreamk.datastoragetest1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import androidx.annotation.Nullable;
+
+public class MySqliteOpenHelper extends SQLiteOpenHelper {
+
+    private static SQLiteOpenHelper mInstance;
+    public static synchronized SQLiteOpenHelper getmInstance(Context context){
+        if(mInstance == null){
+            mInstance = new MySqliteOpenHelper(context, "myDatabase1",null,1);
+        }
+        return mInstance;
+    }
+
+    private MySqliteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table myTable1(" + "_id integer primary key autoincrement," +
+                "username text, uNumber text,"+ "uColor integer," + "uMessage text," + "permitted intege,r" + "value1 integer)";
+
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
